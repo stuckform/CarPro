d54d8d6f4e46f0e95fff2ef4e93894b3ba7a7385
stuckform <75707524+stuckform@users.noreply.github.com>
2021-01-22 16:00:38 -0500

    Add project files.

---
+++ Models/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarPro.Models
+{
+    public class Car
+    {
+        [Required]
+
+        public string Make { get; set; }
+
+        [Required]
+
+        public string Model { get; set; }
+
+        [Required]
+        [Range(1900, 2030)]
+        public int Year { get; set; }
+
+        public int Mileage { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public int Price { get; set; }
+
+        [Required]
+
+        public string Color { get; set; }
+
+
+        //Navigation
+        public int LotId { get; set; }
+
+        public virtual Car Car { get; set;  }
+    }
+}
---
+++ Models/Lot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarPro.Models
+{
+    public class Lot
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        [Display(Name = "Zip Code")]
+        [Range(10000, 99999)]
+        public int ZipCode { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Phone Number")]
+        public int PhoneNum { get; set; }
+
+        public string Manager { get; set; }
+
+        public virtual ICollection<Car> Cars { get; set; }
+
+
+    }
+}
